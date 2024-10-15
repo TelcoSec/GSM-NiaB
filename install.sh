8763a6b7aacb409f061fab68bbcfd1c86ed2c5f5
RFS <7137874+rfs85@users.noreply.github.com>
2024-10-15 23:39:26 +0100

    ff

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 sudo apt install extrepo
 sudo extrepo enable osmocom-latest
 sudo apt update -y
-sudo apt install -y  telnet iptables git limesuite 
+sudo apt install -y telnet iptables git limesuite bladerf
 sudo apt install -y osmo-hlr  osmo-msc  osmo-mgw  osmo-stp  osmo-bsc  osmo-ggsn osmo-sgsn osmo-bts-trx osmo-trx-lms osmo-pcu osmo-cbc osmo-cbc-utils
