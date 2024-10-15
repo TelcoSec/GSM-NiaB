b43016f2c25814931afcdc047f303d62776c9861
RFS <7137874+rfs85@users.noreply.github.com>
2024-10-15 23:24:53 +0100

    rfs

--- install.sh
+++ install.sh
@@ -1,22 +1,8 @@
 
 # Debian 12 - RPI4
-
+## 15/10/2024
 sudo apt install extrepo
 sudo extrepo enable osmocom-latest
 sudo apt update -y
-sudo apt install telnet         \
-            iptables       \
-            limesuite      \
-            osmo-hlr       \
-            osmo-msc       \
-            osmo-mgw       \
-            osmo-stp       \
-            osmo-bsc       \
-            osmo-ggsn      \
-            osmo-sgsn      \
-            osmo-bts-trx   \
-            osmo-trx-lms   \
-            osmo-pcu       \
-            osmo-cbc       \
-            osmo-cbc-utils -y
-
+sudo apt install -y  telnet iptables git limesuite 
+sudo apt install -y osmo-hlr  osmo-msc  osmo-mgw  osmo-stp  osmo-bsc  osmo-ggsn osmo-sgsn osmo-bts-trx osmo-trx-lms osmo-pcu osmo-cbc osmo-cbc-utils
