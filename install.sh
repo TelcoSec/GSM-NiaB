df00c6dee3d7cb09cfc97973344f4ff8097347ea
RFS <7137874+rfs85@users.noreply.github.com>
2024-10-16 18:50:45 +0100

    sdf

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 
 # Debian 12 - RPI4
 ## 15/10/2024
+
+
+
+
+sudo hostnamectl set-hostname 2g-b0x.telecom.rfs
+
 sudo apt install extrepo
 sudo extrepo enable osmocom-latest
 sudo apt update -y
@@ -13,7 +19,7 @@ sudo apt install -y osmo-hlr osmo-msc osmo-mgw osmo-stp osmo-bsc osmo-ggsn osmo-
 # Asterix
 git clone https://github.com/asterisk/asterisk.git
 cd asterisk
-sudo apt install uuid-dev libedit* libxml2 libxml2-dev
+sudo apt install uuid-dev libedit* libxml2 libxml2-dev libsqlite3-dev
 ./configure --with-jansson-bundled
 make
 sudo make install
