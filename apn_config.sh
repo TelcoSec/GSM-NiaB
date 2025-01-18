7e27aaa5ff654e626c0d198de0afc56f484bbf59
RFS <7137874+rfs85@users.noreply.github.com>
2025-01-18 05:49:00 +0000

    apn

---
+++ apn_config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo " usage: ./apn_config.sh eth0"
+dev="${1:-eth0}"
+nr="$(ip a | grep "^[0-9]*: $dev" | wc -l)"
+name="$(echo "$dev" | sed 's/[^0-9a-fA-F]//g' | head -c 1)"
+mac="ac:ac:1a:b0:a0:$name$nr"
+set -e -x
+sudo ip link add link $dev address $mac $dev.$nr type macvlan
+sudo dhclient $dev.$nr
+ip addr show dev $dev.$nr
